15cedbb95b9c702984ac1ab6ff9d3fea0b761a6e
Md Saddam Hossain <gpcshonchoy@gmail.com>
2026-07-06 18:13:14 +0600

    feat: compose final homepage sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,26 @@ import {
   BestSellersSection,
   WhyChooseUsSection,
   CustomerReviewsSection,
+  GallerySection,
+  NewsletterSection,
 } from "@/components/sections";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+
       <FeaturedCollectionsSection />
+
       <BestSellersSection />
+
       <WhyChooseUsSection />
+
       <CustomerReviewsSection />
+
+      <GallerySection />
+
+      <NewsletterSection />
     </>
   );
 }
